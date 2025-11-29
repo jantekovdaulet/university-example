cbef20f42889bc2b8a02a21e7bb1a8eccc0869b6
Jantekov Daulet <117241437+jantekovdaulet@users.noreply.github.com>
2025-11-29 12:40:09 +0500

    Add click event to helloBtn to display message

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("helloBtn").addEventListener("click", () => {
+    const message = document.getElementById("message");
+    message.textContent = "Hello from GitHub Pages and Cloudflare Pages!";
+});
